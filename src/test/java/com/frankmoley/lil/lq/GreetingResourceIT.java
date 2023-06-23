680cfbcfba32a1e7acfaf607ab6ebd9e52ee1f09
quarkusio <no-reply@quarkus.io>
2023-06-23 14:47:21 +0000

    Initial commit

---
+++ src/test/java/com/frankmoley/lil/lq/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.frankmoley.lil.lq;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
